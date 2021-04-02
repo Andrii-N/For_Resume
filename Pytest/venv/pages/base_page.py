1cf729ab9c24a91ef03f5bdf9257e2600702fbfa
Andrii-N <naionma7600@gmail.com>
2021-04-02 12:14:50 +0300

    Improvement of Bas page object with Login link tests

--- Pytest/venv/pages/base_page.py
+++ Pytest/venv/pages/base_page.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 import math
 
 
@@ -13,6 +14,11 @@ class BasePage():
         self.browser = browser
         self.url = url
         #self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
     def open(self):
         self.browser.get(self.url)
 
@@ -22,6 +28,10 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK),\
+            "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
